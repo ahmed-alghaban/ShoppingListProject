b97ac1163d0a6bf5c56a9958ad484386c9450def
ahmad alghaban <hamood-xpy@outlook.sa>
2022-08-26 22:00:49 +0300

    Initial Commit

---
+++ ShoppinList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppinList
+//
+//  Created by ahmad alghaban on 28/01/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
